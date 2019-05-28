429763c2d07c198f70e9dd15e65d6f161cd26d1b
johnlee47 <yohannesfassil@gmail.com>
2019-05-29 01:52:58 +0300

    Fixed bugs in pipe.c

--- Pipes/Pipes.c
+++ Pipes/Pipes.c
@@ -1,43 +1,55 @@
-#include<stdio.h>
-#include<unistd.h>
-
-int main() {
-   int pipefds1[2], pipefds2[2];
-   int returnstatus1, returnstatus2;
-   int pid;
-   char pipe1writemessage[20] = "Enkokelesh";
-   char pipe2writemessage[20] = "Menawekeleh";
-   char readmessage[20];
-   
-   if (pipe(pipefds1) == -1) {
-      printf("Unable to create pipe 1 \n");
-      return 1;
-   }
-   
-   if (pipe(pipefds2) == -1) {
-      printf("Unable to create pipe 2 \n");
-      return 1;
-   }
-   pid = fork();
-   
-   for (int i = 0; i < 3; i++){
-
-      if (pid != 0){ // Parent process {
-         close(pipefds1[0]); // Close the unwantead pipe1 read side
-         close(pipefds2[1]); // Close the unwanted pipe2 write side
-         printf("In Parent: Writing to pipe 1 -%s\n", pipe1writemessage);
-         write(pipefds1[1], pipe1writemessage, sizeof(pipe1writemessage));
-         read(pipefds2[0], readmessage, sizeof(readmessage));
-         printf("In Parent: Reading from pipe 2 –  %s\n", readmessage);
-      } else { //child process
-         close(pipefds1[1]); // Close the unwanted pipe1 write side
-         close(pipefds2[0]); // Close the unwanted pipe2 read side
-         read(pipefds1[0], readmessage, sizeof(readmessage));
-         printf("In Child: Reading from pipe 1 –  %s\n", readmessage);
-         printf("In Child: Writing to pipe 2 –  %s\n", pipe2writemessage);
-         write(pipefds2[1], pipe2writemessage, sizeof(pipe2writemessage));
-      }
-   }
-   
-   return 0;
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+static int pipefd1[2],pipefd2[2];
+void TELL_WAIT(void){
+    if(pipe(pipefd1)<0 || pipe(pipefd2)<0){
+        perror("pipe not created");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void TELL_PARENT(void){
+//waitting to complete
+    printf("Child send message to parent: Wusha\n");
+}
+
+void WAIT_PARENT(void){
+//waitting to complete
+    printf("Child receive message from parent: Sikome achir sikemete rejeme\n");
+}
+
+void TELL_CHILD(void){
+//waitting to complete
+    printf("Parent send message to child: Enkokelesh\n");
+}
+
+void WAIT_CHILD(void){
+//Waitting to complete
+    printf("Parent receive message from child: Menawekeleh\n");
+}
+
+void TELL_BOTH(void){
+    printf("Parent receive message from child!\n");
+}
+
+int main(int argc, char *argv[]){
+    TELL_WAIT();
+    pid_t pid;
+    pid = fork();
+    alarm(3);
+    if(pid!=0){
+        while(1){
+            sleep(rand()%2+1);
+            TELL_CHILD();
+            WAIT_CHILD();
+        }
+    }else{
+        while(1){
+            sleep(rand()%2+1);
+            WAIT_PARENT();
+            TELL_PARENT();
+        }
+    }
+    return 0;
 }
